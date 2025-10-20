a09a8580fbe4a0c8d1541867a29b662d8e3eb241
KouroshM2006 <kouroshmaissamy@gmail.com>
2025-10-20 09:23:28 -0400

    project initialized

---
+++ src/lab07_kourosh/Lab07_kourosh.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab07_kourosh;
+
+//github link: https://github.com/KouroshM2006/Lab07_Kourosh.git
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author kouro
+ */
+public class Lab07_kourosh extends Application {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+    }
+    
+}
