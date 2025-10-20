718203443f2ae07766dcfbeff115e6cc9e23d2b1
KouroshM2006 <kouroshmaissamy@gmail.com>
2025-10-20 09:42:32 -0400

    root nodes created

--- src/lab07_kourosh/Lab07_kourosh.java
+++ src/lab07_kourosh/Lab07_kourosh.java
@@ -6,7 +6,12 @@ package lab07_kourosh;
 
 //github link: https://github.com/KouroshM2006/Lab07_Kourosh.git
 
+import javafx.scene.control.*;
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 /**
@@ -25,6 +30,26 @@ public class Lab07_kourosh extends Application {
 
     @Override
     public void start(Stage stage) {
+        BorderPane root = new BorderPane();
+        
+        Pane middlepaPane = new Pane();
+        
+        Button startButton = new Button("Start");
+        Button resetButton = new Button("Reset");
+        Button exitButton = new Button("Exit");
+        
+        exitButton.setOnAction(e -> stage.close());
+        
+        HBox buttonsBox = new HBox();
+        buttonsBox.getChildren().addAll(startButton, resetButton, exitButton);
+        buttonsBox.setAlignment(Pos.CENTER);
+        
+        root.setBottom(buttonsBox);
+        root.setCenter(middlepaPane);
+        
+        Scene scene = new Scene(root, 500, 500);
+        stage.setScene(scene);
+        stage.show();
     }
     
 }
